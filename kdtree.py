22f2c359379f247d70488d3d022a7057a2cd5525
Maciej <maciek2tr99@gmail.com>
2019-12-03 17:10:53 +0100

    Debug - kdtree

--- kdtree.py
+++ kdtree.py
@@ -1,12 +1,13 @@
 import numpy as np
 from point import Point
 from scope import Scope
-# import generator
 import simple_visualiser
 
+
 class Node:
-    def __init__(self, p, left=None, right=None):
-        self.point = Point(p)
+    def __init__(self, point = None, line=None, left=None, right=None):
+        self.point = Point(point) if point is not None else None
+        self.line = line
         self.left = left
         self.right = right
 
@@ -17,11 +18,14 @@ def _construct(points, depth=0):
     if len(points) == 1:
         return Node(points[0])
     k = len(points) // 2
-    np.partition(points, k, order='x' if depth % 2 == 0 else 'y')
+    indice = depth % 2
+    m = int(np.median([i[indice] for i in points]))
+    left_slice  = list(filter(lambda x: x[indice] <= m, points))
+    right_slice = list(filter(lambda x: x[indice] > m, points))
     return Node(
-        points[k],
-        _construct(points[:k], depth + 1),
-        _construct(points[k:], depth + 1)
+        line = m,
+        left = _construct(left_slice, depth + 1),
+        right = _construct(right_slice , depth + 1)
     )
 
 
@@ -41,27 +45,23 @@ def report_subtree(node):
 
 
 def check_child(node, child, actual_scope, depth, scope):
+    # print("Check ", 'left' if child==node.left else 'right' , depth)
     if child is not None:
         new_scope = Scope()
         new_scope.copy(actual_scope)
 
         if depth % 2 == 0 and node.left == child:
-            new_scope.common(x_high=node.point.x)
+            new_scope.common(x_high=node.line) #.point.x)
         elif depth % 2 == 1 and node.left == child:
-            new_scope.common(y_high=node.point.y)
+            new_scope.common(y_high=node.line) #point.y)
         elif depth % 2 == 0 and node.right == child:
-            new_scope.common(x_low=node.point.x)
+            new_scope.common(x_low=node.line) #point.x)
         else:
-            new_scope.common(y_low=node.point.y)
+            new_scope.common(y_low=node.line) #point.y)
 
-        # print("Node : ", node.point.get_tuple())
-        # print("Scope: ", scope.x_low, scope.x_high)
-        # print("Actua: ", actual_scope.x_low, actual_scope.x_high)
-        # print("New_s: ", new_scope.x_low, new_scope.x_high)
         if scope.contains(new_scope):
             return report_subtree(child)
         elif scope.intersects(new_scope):
-            print("Y")
             return _search(child, scope, new_scope, depth + 1)
         return None
 
@@ -79,41 +79,40 @@ def _search(node, scope, actual_scope=Scope(), depth=0):
     res = check_child(node, node.right, actual_scope, depth, scope)
     if res is not None:
         result += res
-    # print("B ",  [p.get_tuple() for p in res])
     return result
 
 
 def print_tree(node, depth=0):
     if node is None:
         return
-    print("depth ", depth, " - ", node.point.get_tuple())
     print_tree(node.left, depth + 1)
     print_tree(node.right, depth + 1)
 
 
-def search(tree, x_low=-np.inf, x_high=np.inf, y_low=-np.inf, y_high=np.inf):
+def search(tree,actual_scope, x_low=-np.inf, x_high=np.inf, y_low=-np.inf, y_high=np.inf):
     scope = Scope(x_low, x_high, y_low, y_high)
-    return [p.get_tuple() for p in _search(tree, scope)]
+    s = _search(tree, scope, actual_scope)
+    return [p.get_tuple() for p in s]
 
 
 def construct(points):
     k = len(points) // 2
-    p = np.array(points, dtype=[('x', np.int), ('y', np.int)])
-    return _construct(p)
+    return _construct(points) , \
+           Scope(min(points, key =lambda x:x[0])[0],
+                 max(points, key =lambda x:x[0])[0],
+                 min(points, key =lambda y:y[1])[1],
+                 max(points, key =lambda y:y[1])[1])
 
 
 if __name__ == '__main__':
-    points_set = [(0, 10), (-10, -10), (10, 10), (10, 0), (-10, 0), (0, -10)]
+    # points_set = [(0, 10), (-10, -10), (10, 10), (10, 0), (-10, 0), (0, -10)]
     # points_set = [(1, 9), (2, 8), (3, 7), (4, 6), (5, 5), (6, 4), (7, 3), (8, 2), (9, 1)]
+    points_set = [(0, 0), (20, 10), (20, 70), (60, 10), (60, 40), (70, 80), (75, 90), (80, 85), (80, 80), (80, 83)]
 
-    kdtree = construct(points_set)
-    s = search(kdtree, x_low=-2, x_high=2)
+    kdtree, act_scope = construct(points_set)
+    s = search(kdtree,act_scope, x_low=10, x_high=100, y_low=20, y_high=80)
     print(s)
 
     plot = simple_visualiser.Plot([simple_visualiser.PointsCollection(points_set),
-                     simple_visualiser.PointsCollection(s, 'red', marker="x")])
+                                   simple_visualiser.PointsCollection(s, 'red', marker="x")])
     plot.draw()
-
-
-    # print_tree(kdtree)
-    # nie dziala xD
--- scope.py
+++ scope.py
@@ -1,18 +1,15 @@
 import numpy as np
 
 
-# def choose(var, val, fun):
-#     if val is not None:
-#         return fun(var, val)
-#     return var
-
-
 class Scope:
     def __init__(self, xl=-np.inf, xh=np.inf, yl=-np.inf, yh=np.inf):
         self.x_low = xl
         self.x_high = xh
         self.y_low = yl
         self.y_high = yh
+
+    def get_tuple(self):
+        return tuple([self.x_low, self.x_high, self.y_low,self.y_high])
 
     def in_scope(self, point):
         return self.x_low <= point.x <= self.x_high and self.y_low <= point.y <= self.y_high
@@ -25,14 +22,7 @@ class Scope:
                 )
 
     def intersects(self, other):
-        print("A self: ", self.x_low, self.x_high, self.y_low, self.y_high)
-        print("Aother: ", other.x_low, other.x_high, other.y_low, other.y_high)
-
-        # return ((self.x_low <= other.x_low <= self.x_high
-        #          or self.x_low <= other.x_high <= self.x_high)
-        #         and (self.y_low <= other.y_low <= self.y_high
-        #              or self.y_low <= other.y_high <= self.y_high))
-        if self.x_low > other.x_high or other.x_low > self.x_low:
+        if self.x_low > other.x_high or other.x_low > self.x_high:
             return False
 
         if self.y_low > other.y_high or other.y_low > self.y_high:
@@ -41,10 +31,6 @@ class Scope:
         return True
 
     def common(self, x_low=None, x_high=None, y_low=None, y_high=None):
-        # self.x_low = choose(self.x_low, x_low, max)
-        # self.y_low = choose(self.y_low, y_low, max)
-        # self.x_high = choose(self.x_high, x_high, min)
-        # self.y_high = choose(self.y_high, y_high, min)
         if x_low is not None:
             self.x_low = max(self.x_low, x_low)
         if x_high is not None:
