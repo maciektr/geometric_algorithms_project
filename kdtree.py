05e0a2c118b9bbacabb3ff55415355c2feb9e48d
Maciej <maciek2tr99@gmail.com>
2019-12-03 22:29:21 +0100

    Add comments to kd-tree and simple-geometry

--- kdtree.py
+++ kdtree.py
@@ -1,16 +1,25 @@
+#########################################
+#      Algorytmy Grafowe 2019/2020      #
+#         Dwuwmiarowe KD-drzewa         #
+#          Stanislaw Denkowski          #
+#          Maciej Tratnowiecki          #
+#########################################
+
+# Import wykorzystywanych modulow
 import numpy as np
-# from point import Point
 from simple_geometry import *
-import simple_visualiser
 
 
+# Klasa reprezentujaca wezel w KD-drzewie
 class Node:
-    def __init__(self, point = None, line=None, left=None, right=None):
+    # Konstruktor klasy
+    def __init__(self, point=None, line=None, left=None, right=None):
         self.point = Point(point) if point is not None else None
         self.line = line
         self.left = left
         self.right = right
 
+    # Funkcja wypisujaca rekurencyjnie, w kolejnosci preorder wszystkie wezly drzewa
     def report_subtree(self):
         if self is None:
             return None
@@ -25,6 +34,7 @@ class Node:
             result += res
         return result
 
+    # Funkcja realizujaca rekurencyjny algorytm przeszukania KD-drzewa na poziomie wezla
     def _search(self, scope, actual_scope=Scope(), depth=0):
         if self is None:
             return None
@@ -40,19 +50,20 @@ class Node:
             result += res
         return result
 
-    def check_child(node, child, actual_scope, depth, scope):
+    # Funkcja pomocnicza powyzszej, uruchamiana dla obu poddrzew wezla
+    def check_child(self, child, actual_scope, depth, scope):
         if child is not None:
             new_scope = Scope()
             new_scope.copy(actual_scope)
 
-            if depth % 2 == 0 and node.left == child:
-                new_scope.common(x_high=node.line)
+            if depth % 2 == 0 and self.left == child:
+                new_scope.common(x_high=self.line)
             elif depth % 2 == 1 and node.left == child:
-                new_scope.common(y_high=node.line)
+                new_scope.common(y_high=self.line)
             elif depth % 2 == 0 and node.right == child:
-                new_scope.common(x_low=node.line)
+                new_scope.common(x_low=self.line)
             else:
-                new_scope.common(y_low=node.line)
+                new_scope.common(y_low=self.line)
 
             if scope.contains(new_scope):
                 return child.report_subtree()
@@ -61,7 +72,12 @@ class Node:
             return None
 
 
+# Klasa enkapsulujaca implementacje kd-drzewa
+# Przechowuje wezel drzewa oraz obszar do ktorego naleza przechowywane punkty
 class Kdtree:
+    # Konstruktor,
+    # oblicza przedzial w jakim zawarte sa punkty przechowywane w drzewie,
+    # wywoluje rekurencyjna funkcje pomocnicza konstruujaca drzewo
     def __init__(self, points):
         self.scope = Scope(min(points, key=lambda x: x[0])[0],
               max(points, key=lambda x: x[0])[0],
@@ -69,6 +85,7 @@ class Kdtree:
               max(points, key=lambda y: y[1])[1])
         self.root = self._construct(points)
 
+    # Wspomniana pomocnicza powyzszej, realizuje konstrukcje drzewa zwracajac wezel - korzen drzewa
     def _construct(self, points, depth=0):
         if len(points) < 1:
             return None
@@ -85,12 +102,14 @@ class Kdtree:
             right = self._construct(right_slice , depth + 1)
         )
 
+    # Funkcja implementujaca algorytm przeszukania kd-drzewa, odwolujaca sie do metod z wezla
     def find(self,  x_low=-np.inf, x_high=np.inf, y_low=-np.inf, y_high=np.inf):
         scope = Scope(x_low, x_high, y_low, y_high)
         res = self.root._search(scope, self.scope)
         return [p.get_tuple() for p in res]
 
 
+# Ponizszy kod nie jest wykorzystywany w implementacji kd-drzewa
 if __name__ == '__main__':
     # points_set = [(0, 10), (-10, -10), (10, 10), (10, 0), (-10, 0), (0, -10)]
     # points_set = [(1, 9), (2, 8), (3, 7), (4, 6), (5, 5), (6, 4), (7, 3), (8, 2), (9, 1)]
@@ -100,6 +119,6 @@ if __name__ == '__main__':
     s = kdtree.find(x_low=10, x_high=100, y_low=20, y_high=80)
     print(s)
 
-    plot = simple_visualiser.Plot([simple_visualiser.PointsCollection(points_set),
-                                   simple_visualiser.PointsCollection(s, 'red', marker="x")])
-    plot.draw()
+    # plot = simple_visualiser.Plot([simple_visualiser.PointsCollection(points_set),
+    #                                simple_visualiser.PointsCollection(s, 'red', marker="x")])
+    # plot.draw()
--- simple_geometry.py
+++ simple_geometry.py
@@ -1,19 +1,32 @@
+#########################################
+#      Algorytmy Grafowe 2019/2020      #
+#           Klasy pomocnicze            #
+#          Stanislaw Denkowski          #
+#          Maciej Tratnowiecki          #
+#########################################
+
+# Import wykorzystywanych modulow
 import numpy as np
 
 
+# Klasa przechowujaca obszar prostokatny w pamieci
 class Scope:
+    # Konstruktor klasy
     def __init__(self, xl=-np.inf, xh=np.inf, yl=-np.inf, yh=np.inf):
         self.x_low = xl
         self.x_high = xh
         self.y_low = yl
         self.y_high = yh
 
-    def get_tuple(self):
-        return tuple([self.x_low, self.x_high, self.y_low,self.y_high])
+    # Funkcja zwracajaca reprezentacje instancji klasy w formie lancucha znakow
+    def __str__(self):
+        return '('+str(self.x_low)+', '+str(self.x_high)+', '+str(self.y_low)+', '+str(self.y_high)+')'
 
+    # Funkcja sprawdzajaca czy punkt podany jako argument nalezy do przechowywanego przedzialu
     def in_scope(self, point):
         return self.x_low <= point.x <= self.x_high and self.y_low <= point.y <= self.y_high
 
+    # Funkcja sprawdzajaca, czy dwa obszary sie w sobie zawieraja
     def contains(self, other):
         return (self.x_low <= other.x_low
                 and self.x_high >= other.x_high
@@ -21,6 +34,7 @@ class Scope:
                 and self.y_high >= other.y_high
                 )
 
+    # Funkcja sprawdzajaca czy dwa obszary sie przecinaja
     def intersects(self, other):
         if self.x_low > other.x_high or other.x_low > self.x_high:
             return False
@@ -30,6 +44,7 @@ class Scope:
 
         return True
 
+    # Funkcja realizujaca operacje przeciecia obszaru z polplaszczyzna
     def common(self, x_low=None, x_high=None, y_low=None, y_high=None):
         if x_low is not None:
             self.x_low = max(self.x_low, x_low)
@@ -40,16 +55,20 @@ class Scope:
         if y_high is not None:
             self.y_high = min(self.y_high, y_high)
 
+    # Funkcja kopiujaca zawartosc innej instancji klasy
     def copy(self, other):
         self.x_low = other.x_low
         self.x_high = other.x_high
         self.y_low = other.y_low
         self.y_high = other.y_high
 
+
+# Klasa przechowujaca punkt na dwuwymiarowej plaszczyznie euklidesowej w pamieci
 class Point:
     def __init__(self, s):
         self.x = s[0]
         self.y = s[1]
 
+    # Funkcja zwracajaca punkt w postaci dwuelementowej krotki
     def get_tuple(self):
         return tuple([self.x, self.y])
